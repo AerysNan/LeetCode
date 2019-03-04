dd0328c657716baaabb1423e81a95bf1f978bdb0
AerysNan <nany16@163.com>
2019-03-04 11:17:49 +0800

    0041

---
+++ 0041.缺失的第一个正数.java
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=41 lang=java
+ *
+ * [41] 缺失的第一个正数
+ *
+ * https://leetcode-cn.com/problems/first-missing-positive/description/
+ *
+ * algorithms
+ * Hard (35.66%)
+ * Total Accepted:    8.4K
+ * Total Submissions: 23.4K
+ * Testcase Example:  '[1,2,0]'
+ *
+ * 给定一个未排序的整数数组，找出其中没有出现的最小的正整数。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,2,0]
+ * 输出: 3
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: [3,4,-1,1]
+ * 输出: 2
+ *
+ *
+ * 示例 3:
+ *
+ * 输入: [7,8,9,11,12]
+ * 输出: 1
+ *
+ *
+ * 说明:
+ *
+ * 你的算法的时间复杂度应为O(n)，并且只能使用常数级别的空间。
+ *
+ */
+class Solution {
+  public int firstMissingPositive(int[] nums) {
+    int length = nums.length;
+    for (int i = 0; i < length; i++) {
+      while (true) {
+        if (nums[i] <= 0 || nums[i] >= length || nums[i] == i + 1 || nums[i] == nums[nums[i] - 1])
+          break;
+        int tmp = nums[i];
+        nums[i] = nums[tmp - 1];
+        nums[tmp - 1] = tmp;
+      }
+    }
+    for (int i = 0; i < length; i++)
+      if (nums[i] != i + 1)
+        return i + 1;
+    return length + 1;
+  }
+}
