81f4d089486cf12a42db736cbde26d2660a9c019
AerysNan <aerysnan@gmail.com>
2019-06-11 16:20:55 +0800

    1108

---
+++ 1108.ip-地址无效化.java
@@ -0,0 +1,10 @@
+/*
+ * @lc app=leetcode.cn id=1108 lang=java
+ *
+ * [1108] IP 地址无效化
+ */
+class Solution {
+  public String defangIPaddr(String address) {
+    return address.replaceAll("\\.", "[.]");
+  }
+}
