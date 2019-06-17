e58403b5cd01c3356369438a9057388c87f95b50
AerysNan <aerysnan@gmail.com>
2019-06-17 21:34:54 +0800

    0009

---
+++ 0009.回文数.java
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=java
+ *
+ * [9] 回文数
+ */
+class Solution {
+  public boolean isPalindrome(int x) {
+    String s = String.valueOf(x);
+    int n = s.length();
+    for (int i = 0; i < n / 2; i++)
+      if (s.charAt(i) != s.charAt(n - 1 - i))
+        return false;
+    return true;
+  }
+}
