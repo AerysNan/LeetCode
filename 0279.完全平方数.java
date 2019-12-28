2815f324f91a6d38b380425b0710cc7cd267ada6
AerysNan <aerysnan@gmail.com>
2019-12-28 14:08:34 +0800

    0279

---
+++ 0279.完全平方数.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=279 lang=java
+ *
+ * [279] 完全平方数
+ */
+
+// @lc code=start
+import java.util.Arrays;
+
+class Solution {
+  public int numSquares(int n) {
+    int[] dp = new int[n + 1];
+    Arrays.fill(dp, Integer.MAX_VALUE);
+    dp[0] = 0;
+    for (int i = 0; i < n; i++)
+      for (int j = 1; i + j * j <= n; j++)
+        dp[i + j * j] = Math.min(dp[i + j * j], dp[i] + 1);
+    return dp[n];
+  }
+}
+// @lc code=end
+
