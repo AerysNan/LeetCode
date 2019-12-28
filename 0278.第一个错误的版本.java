54c18364c82bad0231723d491b82b93bebc83601
AerysNan <aerysnan@gmail.com>
2019-12-28 14:05:56 +0800

    0278

---
+++ 0278.第一个错误的版本.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=278 lang=java
+ *
+ * [278] 第一个错误的版本
+ */
+
+// @lc code=start
+/*
+ * The isBadVersion API is defined in the parent class VersionControl. boolean isBadVersion(int
+ * version);
+ */
+
+public class Solution extends VersionControl {
+  public int firstBadVersion(int n) {
+    int l = 1, r = n;
+    while (l < r) {
+      int mid = (r - l) / 2 + l;
+      if (isBadVersion(mid))
+        r = mid;
+      else
+        l = mid + 1;
+    }
+    return l;
+  }
+}
+// @lc code=end
+
