1df97ec5cabbf71bfc65772e3024221d60069da7
AerysNan <aerysnan@gmail.com>
2020-05-07 18:09:43 +0800

    0507

---
+++ 0507.完美数.java
@@ -0,0 +1,13 @@
+/*
+  * @lc app=leetcode.cn id=507 lang=java
+  *
+  * [507] 完美数
+  */
+
+// @lc code=start
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        return num == 6 || num == 28 || num == 496 || num == 8128 || num == 33550336;
+    }
+}
+// @lc code=end
