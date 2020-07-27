474d9ab5b75e52593dc105b9b785336998585653
AerysNan <aerysnan@gmail.com>
2020-07-27 15:34:15 +0800

    0726

---
+++ 0726.原子的数量.java
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=726 lang=java
+ *
+ * [726] 原子的数量
+ */
+
+// @lc code=start
+import java.util.Stack;
+import java.util.TreeMap;
+
+class Solution {
+    public String countOfAtoms(String formula) {
+        int N = formula.length();
+        Stack<TreeMap<String, Integer>> stack = new Stack<>();
+        stack.push(new TreeMap<>());
+        for (int i = 0; i < N;) {
+            if (formula.charAt(i) == '(') {
+                stack.push(new TreeMap<>());
+                i++;
+            } else if (formula.charAt(i) == ')') {
+                TreeMap<String, Integer> top = stack.pop();
+                int iStart = ++i, multiplicity = 1;
+                while (i < N && Character.isDigit(formula.charAt(i)))
+                    i++;
+                if (i > iStart)
+                    multiplicity = Integer.parseInt(formula.substring(iStart, i));
+                for (String c : top.keySet()) {
+                    int v = top.get(c);
+                    stack.peek().put(c, stack.peek().getOrDefault(c, 0) + v * multiplicity);
+                }
+            } else {
+                int iStart = i++;
+                while (i < N && Character.isLowerCase(formula.charAt(i)))
+                    i++;
+                String name = formula.substring(iStart, i);
+                iStart = i;
+                while (i < N && Character.isDigit(formula.charAt(i)))
+                    i++;
+                int multiplicity = i > iStart ? Integer.parseInt(formula.substring(iStart, i)) : 1;
+                stack.peek().put(name, stack.peek().getOrDefault(name, 0) + multiplicity);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (String name : stack.peek().keySet()) {
+            result.append(name);
+            int multiplicity = stack.peek().get(name);
+            if (multiplicity > 1)
+                result.append("" + multiplicity);
+        }
+        return new String(result);
+    }
+}
+// @lc code=end
