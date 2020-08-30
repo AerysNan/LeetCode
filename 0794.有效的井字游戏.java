f7ff66a4f8f09fc81f00e3f99073d7d7935c6f98
AerysNan <aerysnan@gmail.com>
2020-08-30 14:21:25 +0800

    0794

---
+++ 0794.有效的井字游戏.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=794 lang=java
+ *
+ * [794] 有效的井字游戏
+ */
+
+// @lc code=start
+class Solution {
+  public boolean validTicTacToe(String[] board) {
+    int xCount = 0, oCount = 0;
+    for (String row : board) {
+      for (char c : row.toCharArray()) {
+        if (c == 'X')
+          xCount++;
+        if (c == 'O')
+          oCount++;
+      }
+    }
+    return !(oCount != xCount && oCount != xCount - 1 || win(board, 'X') && oCount != xCount - 1
+        || win(board, 'O') && oCount != xCount);
+  }
+
+  public boolean win(String[] B, char P) {
+    for (int i = 0; i < 3; ++i) {
+      if (P == B[0].charAt(i) && P == B[1].charAt(i) && P == B[2].charAt(i))
+        return true;
+      if (P == B[i].charAt(0) && P == B[i].charAt(1) && P == B[i].charAt(2))
+        return true;
+    }
+    if (P == B[0].charAt(0) && P == B[1].charAt(1) && P == B[2].charAt(2))
+      return true;
+    if (P == B[0].charAt(2) && P == B[1].charAt(1) && P == B[2].charAt(0))
+      return true;
+    return false;
+  }
+}
+// @lc code=end
